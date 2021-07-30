9e389d1c7d051c82c24f86eb640abda69ce4e729
fibletype <fibletyp@gmail.com>
2021-07-30 13:24:09 +0300

    add html output

--- prusti-pretti-report/src/main.rs
+++ prusti-pretti-report/src/main.rs
@@ -5,16 +5,18 @@ use pulldown_cmark::{Parser, Options, html};
 use serde_json::json;
 use serde_derive::{Deserialize, Serialize};
 
+
 #[derive(Serialize, Deserialize)]
-struct Tests {
-    ok_ : bool,
-    func : String,
-    line : String, 
-    symbl : u16
+struct Text {
+    text : String
 }
 
 #[derive(Serialize, Deserialize)]
 struct Spans {
+    file_name : String,
+    column_start : u16,
+	column_end : u16,
+    text : Vec<Text>,
     line_start : u64,
     line_end : u64
 }
@@ -28,21 +30,17 @@ struct Children {
 #[derive(Serialize, Deserialize)]
 struct VerificationResult {
     message : String,
+    spans : Vec<Spans>,
     children : Vec<Children>
 }
 
-fn init_template (prog : &String, status : bool, start : u64, end : u64) -> String {
+fn init_template (status : bool) -> String {
     
     if status {
-        //Программа ++ prog ++ успешно верифицирована. В этом файле вы можете увидеть доказательства соответствия спецификации
-        format!("Program () {} has been verified sucsesfully", &prog)
+        format!("Project has been verified successfully.")
     }
     else {
-
-        format!("Program () {} don't satisfies specification. look at line {}-{}", &prog, start, end)
-        /* .push_str(prog) */
-        //Программа ++ prog ++ не соответствует спецификации. 
-        //Обратите внимание на ++ строки и пре/пост конидшоны.
+            format!("Program doesn't satisfy specification.")
     }
 }
 
@@ -57,15 +55,34 @@ fn main() -> Result<(), std::io::Error> {
         f.read_to_string(&mut contents)
     }).unwrap();
     let info : VerificationResult = serde_json::from_str(&contents)?;
-    
-
-    println!("{}", info.children[1].message);
-
     let prog_name = "example".to_string();
+    if contents == "" {   
+        let mut init_str = init_template(true);
+        let parser = Parser::new_ext(&init_str, options);
+        let mut html_output = String::new();
+        html::push_html(&mut html_output, parser);
+        let mut file = File::create("ex.html");
+        if let Ok(mut file) = file {
+        file.write(&html_output.as_bytes());
+        }     
+            std::process::exit(1)
+    }
 
-    let status = !info.message.contains("error");
-    println!("{}", status);
-    let init_str = init_template(&prog_name, status, info.children[1].spans[0].line_start,info.children[1].spans[0].line_end);
+    let mut init_str = init_template( false);
+    
+    for i in info.children.iter() {
+        if i.message.contains("error") {
+            for j in i.spans.iter() {
+                init_str += &format!("\n\n in {} \n\n form line {} to line {} ...", j.file_name, j.line_start, j.line_end);
+                for k in j.text.iter() {
+                    init_str += &format!("\n\n {} ", k.text);
+                }
+            }
+        } else if i.message.contains("counterexample"){
+                init_str += &format!("\n\n {}", i.message.to_string()); 
+            }
+    }
+    init_str += &format!("\n\n {}", info.spans[0].text[0].text); //Very Bad
     let parser = Parser::new_ext(&init_str, options);
     let mut html_output = String::new();
     html::push_html(&mut html_output, parser);
@@ -75,48 +92,11 @@ fn main() -> Result<(), std::io::Error> {
     }
 
 
-
-
-
-
-    let data = r#"
-    {
-        "ok_": true,
-        "func": "Alestra",
-        "line": "12",
-        "symbl": 13,
-        "xyi": "sdfsdf"
-    }"#;
-    
-    let test : Tests = serde_json::from_str(data)?;
-    // Convert to a string of JSON and print it out
-    println!("Poshel naxyi {}", test.ok_);
-
-    // Set up options and parser. Strikethroughs are not part of the CommonMark standard
-    // and we therefore must enable it explicitly.
-   /*  let mut options = Options::empty();
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-    let status = true; //need for prusty
-    let prog_name = "example".to_string(); //need form prog name
-    let init_str = init_template(&prog_name, status);
-    let parser = Parser::new_ext(&init_str, options);
-
-    // Write to String buffer.
-    let mut html_output = String::new();
-    html::push_html(&mut html_output, parser); */
-    //html::write_html(, iter)
-    // Check that the output is what we expected.
-/*     let expected_html = "<p>Hello world, this is a <del>complicated</del> <em>very simple</em> example.</p>\n";
-    assert_eq!(expected_html, &html_output); */
-
-    
     Ok(())
 }
 
 
 
-// Напиать функцию, которая принемает ссылку на слайс от вектора и что-нибудь с ним делает.
-// Передать мутабельную ссылку и два раза из мейн вызвать эту функцию.
 // Что будет, если он не может создать контрпример
 // Create dot
 // обработать ошибку при открытии файла
